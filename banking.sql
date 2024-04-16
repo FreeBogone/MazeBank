d76277a0bce994727053278c88b45164ee33b559
strangerthandev115 <139648342+strangerthandev115@users.noreply.github.com>
2024-04-16 11:38:27 -0500

    Git Ignore and Banking Update

--- banking.sql
+++ banking.sql
@@ -13,7 +13,18 @@ CREATE TABLE Users(
 -- @block
 CREATE TABLE Accounts(
     id INT AUTO_INCREMENT,
-    balance DECIMAL(10, 2) NOT NULL,
+    checking_balance DECIMAL(10, 2) NOT NULL,
+    savings_balance DECIMAL(10, 2) NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES Users(id)
+);
+
+-- @block
+CREATE TABLE History(
+    id INT AUTO_INCREMENT,
+    transaction_type VARCHAR(255) NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
     user_id INT NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY(user_id) REFERENCES Users(id)
